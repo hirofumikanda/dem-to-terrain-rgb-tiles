5677a0bd72e4b89f3b73e2e32df9c256fbf0a4cc
Hirofumi Kanda <nandakandahirofumi322@yahoo.co.jp>
2025-06-13 08:08:07 +0900

    mbtiles登録用のmetadata.sql追加

---
+++ metadata.sql
@@ -0,0 +1,7 @@
+INSERT OR REPLACE INTO metadata (name, value) VALUES ('id', 'terrain_rgb');
+INSERT OR REPLACE INTO metadata (name, value) VALUES ('name', 'terrain_rgb');
+INSERT OR REPLACE INTO metadata (name, value) VALUES ('format', 'png');
+INSERT OR REPLACE INTO metadata (name, value) VALUES ('minzoom', '0');
+INSERT OR REPLACE INTO metadata (name, value) VALUES ('maxzoom', '14');
+INSERT OR REPLACE INTO metadata (name, value) VALUES ('type', 'overlay');
+INSERT OR REPLACE INTO metadata (name, value) VALUES ('version', '1.0');
